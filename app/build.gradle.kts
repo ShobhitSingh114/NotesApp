b0213a02a52fbf1244c9520c19c0e03ef6121286
Shobhit <shobhitsingh1149201@gmail.com>
2024-03-07 20:46:59 +0530

    NotesScreen added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,9 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+//    implementation("androidx.compose.material3:material3")
+//    implementation("androidx.compose.material3:material3-android:1.2.0")
+    implementation("androidx.compose.material3:material3-android:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -70,10 +72,10 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Compose dependencies
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation ("androidx.navigation:navigation-compose:2.7.7")
-    implementation ("androidx.compose.material:material-icons-extended:1.6.2")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
+//    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+//    implementation ("androidx.navigation:navigation-compose:2.7.7")
+//    implementation ("androidx.compose.material:material-icons-extended:1.6.2")
+//    implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // Coroutines
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
